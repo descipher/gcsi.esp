d2503f211c4240044fa0f6d4bf0879ef405fdefb
Mike La Spina <mike.laspina@shaw.ca>
2019-12-01 20:48:49 -0600

    Update GCWebApp.ino

--- GCWebApp.ino
+++ GCWebApp.ino
@@ -1,3 +1,27 @@
+/**
+ *	File:       	GCWebApp.ino
+ *	Version:  	1.0
+ *	Date:       	2019
+ *	License:	GPL v3
+ *	Description:	Geiger.Counter.1 esp8266 main code
+ *	Project:	Geiger.Counter.1, WiFi, Logging, USB Interface and low power field operations.
+ *
+ *  	Copyright 2019 by Gelidus Research Inc, mike.laspina@gelidus.ca
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #define ARDUINOJSON_USE_LONG_LONG 1
 #include <ArduinoJson.h>
 #include "NTPClient.h"
